6aeac819dfbed997b216bc037eafaa83f1e2fd37
rs <r.salerno@reply.it>
2022-03-14 22:41:49 +0100

    add user commands: list, setname, whoami

--- apps/chat_server/src/chat_server_manager.erl
+++ apps/chat_server/src/chat_server_manager.erl
@@ -8,43 +8,57 @@ loop(Socket, Pid) ->
     case gen_tcp:recv(Socket, 0) of
         {ok, Data} ->
             io:format("~s", [Data]),
-            parse_command(Data, Pid),
-            gen_tcp:send(Socket, Data),
+            case manage_cmd_get_resp(Data, Pid) of
+              {ok, Resp} -> gen_tcp:send(Socket, Resp);
+              {ok, _} -> ok
+            end,
             loop(Socket, Pid);
         {error, closed} ->
             client_disconnection(Pid),
             ok
     end.
 
-parse_command(Data, Pid) ->
-  % Cmd_user_setname = "<<user,setname>>",
-  % Cmd_user_list = "<<user,list>>",
-  % % wip
-  % "<<user,disconnect>>",
-  % "<<msg,send>>",
-  % "<<room,list>>",
-  % "<<room,create>>",
-  % "<<room,leave>>",
-  % "<<room,delete>>",
+manage_cmd_get_resp(Data, Pid) ->
   Str = binary_to_list(string:chomp(Data)),
   case Str of
     "<<user,setname>>" ++ Name ->
       cmd_user_setname(Pid, Name);
     "<<user,list>>" ++ B ->
       cmd_user_list();
-    _ -> 
-      io:format("[cmd] _~n",[]),
-      ok
+    "<<user,whoami>>" ++ C ->
+      cmd_whoami(Pid);
+    _ ->
+      cmd_default(Str)
   end.
 
 cmd_user_list() ->
   io:format("[cmd] <<user,list>>~n",[]),
   S = chat_server_users:get_state(),
-  io:format("~p~n",[S]).
+  io:format("~p~n",[S]),
+  {current_state, State} = S,
+  Pred = fun(K,V) -> io_lib:char_list(V) end,
+  Filtered = maps:filter(Pred,State),
+  Keys = maps:values(Filtered),
+  io:format("~p~n",[Keys]),
+  Resp = "users:" ++ string:join(Keys,",") ++ "\n",
+  io:format("~s~n",[Resp]),
+  {ok, Resp}.
 
 cmd_user_setname(Pid, Name) ->
   io:format("[cmd] name: ~s~n",[Name]),
-  chat_server_users:put(Pid, Name).
+  chat_server_users:put(Pid, Name),
+  {ok, "ok\n"}.
+
+cmd_whoami(Pid) ->
+  Val = chat_server_users:get(Pid),
+  case io_lib:char_list(Val) of
+    true -> {ok, Val ++ "\n"};
+    false -> {ok, "name is not set yet\n"}
+  end.
+
+cmd_default(Str) ->
+  io:format("[str] ~s~n",[Str]),
+  {ok, Str ++ "\n"}.
 
 client_disconnection(Pid) ->
   chat_server_users:delete(Pid),
